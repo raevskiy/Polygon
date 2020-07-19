e8c7ef962383610f454823c498e8671ee3fda20c
raevskiy <raevskiy-s-u@yandex.ru>
2020-07-19 22:48:12 +0300

    Opsive items equip listener, for dialogues

---
+++ MonoBehaviours/Interaction/ItemEquipListener.cs
@@ -0,0 +1,33 @@
+﻿using Opsive.ThirdPersonController;
+using UnityEngine;
+
+namespace KopliSoft.Interaction
+{
+    public class ItemEquipListener : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject item;
+        [SerializeField]
+        private string flowchartName;
+        [SerializeField]
+        private Fungus.Flowchart flowchart;
+        [SerializeField]
+        private string blockName;
+
+        void Start()
+        {
+            if (flowchart == null && flowchartName != null && flowchartName.Trim().Length != 0)
+            {
+                flowchart = GameObject.Find(flowchartName).GetComponent<Fungus.Flowchart>();
+            }
+
+            EventHandler.RegisterEvent(gameObject, "OnInventoryItemEquipping", OnItemEquipping);
+        }
+
+        private void OnItemEquipping()
+        {
+            flowchart.ExecuteBlock(blockName);
+            EventHandler.UnregisterEvent(gameObject, "OnInventoryItemEquipping", OnItemEquipping);
+        }
+    }
+}
